974b44d488e59583a04731b87b3d43d837538fbc
klaus mikaelson <love20141002@126.com>
2018-11-26 00:05:36 +0800

    [feature] init cloud test

---
+++ demo-eureka-client/src/main/java/com/klaus/demoeurekaclient/DemoEurekaClientApplication.java
@@ -0,0 +1,28 @@
+package com.klaus.demoeurekaclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class DemoEurekaClientApplication {
+
+    @Value("${server.port}")
+    private int port;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEurekaClientApplication.class, args);
+    }
+
+
+    @GetMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+}
